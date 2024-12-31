3d1d8fd6a973b21a3d311d506b876b7388629336
mrmiles94 <aniefiokasuquo33@gmail.com>
2024-12-31 23:49:08 +0100

    saving

---
+++ app/_layout.jsx
@@ -0,0 +1,14 @@
+import { StatusBar } from 'expo-status-bar';
+import { View, Text} from 'react-native';
+
+export default function RootLayout() {
+ 
+
+  return (
+    <View >
+      <Text className='text-3xl'>
+        Welcome home!
+      </Text>
+    </View>
+  );
+}
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./app/**/*/{js,jsx,ts,tsx}",
+    "./components/**/*.{js,sjx,ts,tsx}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
